811e66d874bd3f5d52274c7d53ff6a0066f6e9bd
hseager <harvey.seager@googlemail.com>
2022-03-15 21:07:46 +0000

    Initial Commit

---
+++ src/gui.tsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './components/App'
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('app')
+// )
+
+// document.getElementsByTagName(
+//   'body'
+// )[0].innerHTML = `node Version: ${process.versions.node}`
+
+alert('nebuli')
+console.log('hi')
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const RemovePlugin = require('remove-files-webpack-plugin')
+
+const srcDir = path.resolve(__dirname, 'src')
+const outputDir = path.resolve(__dirname, 'dist')
+
+const commonConfig = {
+  output: {
+    path: outputDir,
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.tsx?$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-typescript',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      },
+    ],
+  },
+}
+
+module.exports = [
+  Object.assign(
+    {
+      target: 'electron-main',
+      entry: { main: './src/main.tsx' },
+    },
+    commonConfig
+  ),
+  Object.assign(
+    {
+      target: 'electron-renderer',
+      entry: { gui: './src/gui.tsx' },
+      plugins: [
+        new RemovePlugin({
+          before: {
+            log: false,
+            include: [outputDir],
+          },
+        }),
+        new HtmlWebpackPlugin({
+          template: path.join(srcDir, 'index.html'),
+          inject: 'body',
+        }),
+      ],
+    },
+    commonConfig
+  ),
+]
